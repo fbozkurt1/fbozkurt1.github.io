f1869a9e04dc59f77daf4b0ea58066065341cd1d
Fuat Bozkurt <fuatbozkurt1@gmail.com>
2019-09-19 00:03:13 +0300

    Add files via upload
    adding website contents

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var btn = $('#backTop');
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').scrollTop(0);
+});
